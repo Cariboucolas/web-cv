af19c1583e76a65d5883eccec5b0fa810af12b53
Colas Durcy <cariboucolas@gmail.com>
2026-07-30 17:37:25 +0200

    perf: rééchantillonne l'avatar et sert des variantes WebP
    L'original faisait 4472x6708 pour 2 Mo, affiché dans un cercle de 160px.
    Le navigateur réduisait en une passe sans filtrage progressif, d'où
    l'aliasing sur les cheveux et la peau.
    
    Trois variantes WebP couvrent @1x à @3x, avec un JPEG en repli. Le poids
    transféré passe de 2 Mo à moins de 60 Ko. L'original est conservé dans
    assets/originals/, qui n'est pas servi.

--- components/organisms/CharacterPanel.vue
+++ components/organisms/CharacterPanel.vue
@@ -2,7 +2,21 @@
   <div class="character-panel">
     <div class="character-avatar">
       <div class="large-avatar">
-        <img src="/images/avatar.jpg" alt="Colas Durcy" class="avatar-img"/>
+        <picture>
+          <source
+              type="image/webp"
+              srcset="/images/avatar-224.webp 224w, /images/avatar-336.webp 336w, /images/avatar-448.webp 448w"
+              sizes="(max-width: 1024px) 110px, 160px"
+          />
+          <img
+              src="/images/avatar-448.jpg"
+              alt="Colas Durcy"
+              class="avatar-img"
+              width="160"
+              height="160"
+              decoding="async"
+          />
+        </picture>
       </div>
     </div>
     <div class="character-info">
